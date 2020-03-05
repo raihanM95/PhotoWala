fcd094a3e52115c8e4a47dd1a905ea66015f1186
Raihan Mahmud <mahmud.raihan@yahoo.com>
2020-03-05 14:32:08 +0600

    IUserService added

---
+++ PhotoWala.Interface/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using PhotoWala.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoWala.Interface
+{
+    public interface IUserService
+    {
+        bool Register(User user);
+        bool Login(string userName, string Password);
+        //
+        bool UpdateInfo(User user);
+        List<User> GetUsers();
+        User GetByID(int id);
+        bool DeleteUser(int id);
+    }
+}
