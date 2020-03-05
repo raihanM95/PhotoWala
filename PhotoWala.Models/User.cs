4cfb76f7eb31440cc6caaf9ed90ad8e7b9dc24e1
Raihan Mahmud <mahmud.raihan@yahoo.com>
2020-03-05 12:18:57 +0600

    User-model added

---
+++ PhotoWala.Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PhotoWala.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(11)]
+        public string Phone { get; set; }
+        [StringLength(20)]
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 5)]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
