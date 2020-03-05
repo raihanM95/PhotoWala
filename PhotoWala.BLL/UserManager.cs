bd9cf36bcf2bab0a0b3a7d60a208c2da7f3ddb26
Raihan Mahmud <mahmud.raihan@yahoo.com>
2020-03-05 15:22:41 +0600

    UserManager added

---
+++ PhotoWala.BLL/UserManager.cs
@@ -0,0 +1,44 @@
+﻿using PhotoWala.Interface;
+using PhotoWala.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoWala.BLL
+{
+    public class UserManager
+    {
+        private readonly IUserService _service;
+
+        public UserManager(IUserService service)
+        {
+            _service = service;
+        }
+
+        public bool Register(User user)
+        {
+            return _service.Register(user);
+        }
+        public bool Login(string userName, string password)
+        {
+            return _service.Login(userName, password);
+        }
+        //
+        public bool UpdateInfo(User user)
+        {
+            return _service.UpdateInfo(user);
+        }
+        public List<User> GetUsers()
+        {
+            return _service.GetUsers();
+        }
+        public User GetByID(int id)
+        {
+            return _service.GetByID(id);
+        }
+        public bool DeleteUser(int id)
+        {
+            return _service.DeleteUser(id);
+        }
+    }
+}
